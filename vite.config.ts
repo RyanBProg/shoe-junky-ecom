c35962894576d2016344d95d1e459dc903f6c17a
Ryan <ryanbprog@gmail.com>
2024-06-08 23:33:45 +0800

    declarations added to typescript css modules

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    modules: {
+      // Customize CSS Modules options here
+      localsConvention: "camelCase",
+      scopeBehaviour: "local",
+      // Additional options can be added here as needed
+    },
+  },
+});
