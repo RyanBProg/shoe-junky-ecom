e1563ee5a2cafd8695a4417b1d196fd5936f0864
Ryan <ryanbprog@gmail.com>
2024-06-09 11:12:11 +0800

    small style corrections

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import "./App.scss";
 import CartButton from "./components/product-page/CartButton";
+import ProductQuantity from "./components/product-page/ProductQuantity";
+import { useState } from "react";
 
 function App() {
+  const [qty, setQty] = useState(0);
+
   return (
     <>
       <div>
         <h1>test</h1>
+        <ProductQuantity qty={qty} setQty={setQty} />
         <CartButton />
       </div>
     </>
