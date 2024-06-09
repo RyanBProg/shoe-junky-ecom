08bd84d18ecd2abd48fbd3c36b8d0fc1feac6beb
Ryan <ryanbprog@gmail.com>
2024-06-09 21:36:10 +0800

    product gallary layout complete: - product page elements scale to screen - mobile & destop gallery view completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import "./App.scss";
 import { useState } from "react";
 import ProductInfo from "./components/product-page/ProductInfo";
+import ProductGallery from "./components/product-page/ProductGallery";
 
 function App() {
   const [qty, setQty] = useState(0);
 
   return (
-    <>
+    <div className="cont__product">
+      <ProductGallery />
       <ProductInfo qty={qty} setQty={setQty} />
-    </>
+    </div>
   );
 }
 
---
+++ src/components/product-page/ProductGallery.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import styles from "./ProductGallery.module.scss";
+import productImage1 from "../../assets/images/image-product-1.jpg";
+import productThum1 from "../../assets/images/image-product-1-thumbnail.jpg";
+import productImage2 from "../../assets/images/image-product-2.jpg";
+import productThum2 from "../../assets/images/image-product-2-thumbnail.jpg";
+import productImage3 from "../../assets/images/image-product-3.jpg";
+import productThum3 from "../../assets/images/image-product-3-thumbnail.jpg";
+import productImage4 from "../../assets/images/image-product-4.jpg";
+import productThum4 from "../../assets/images/image-product-4-thumbnail.jpg";
+import leftArrow from "../../assets/images/icon-previous.svg";
+import rightArrow from "../../assets/images/icon-next.svg";
+
+export default function ProductGallery() {
+  return (
+    <div className={styles.cont__gallery}>
+      <img
+        className={styles.product__img}
+        src={productImage1}
+        alt="product image"
+      />
+      <button className={styles.btn__prev}>
+        <img src={leftArrow} alt="previous image" />
+      </button>
+      <button className={styles.btn__next}>
+        <img src={rightArrow} alt="next image" />
+      </button>
+      <div className={styles.thumbnails}>
+        <img src={productThum1} alt="product image thumbnail" />
+        <img src={productThum2} alt="product image thumbnail" />
+        <img src={productThum3} alt="product image thumbnail" />
+        <img src={productThum4} alt="product image thumbnail" />
+      </div>
+    </div>
+  );
+}
