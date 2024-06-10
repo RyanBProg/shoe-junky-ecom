85edf0e87adcb263bbe062b37c2a4f913709585d
Ryan <ryanbprog@gmail.com>
2024-06-10 15:03:07 +0800

    svg styling fixed: - svgs directly added to the jsx

--- src/components/product-page/Lightbox.tsx
+++ src/components/product-page/Lightbox.tsx
@@ -1,8 +1,5 @@
 import styles from "./Lightbox.module.scss";
 import { ProductImage } from "../../utils/types/productPage";
-import leftArrow from "../../assets/images/icon-previous.svg";
-import rightArrow from "../../assets/images/icon-next.svg";
-import exitIcon from "../../assets/images/icon-close.svg";
 
 type Props = {
   setLightboxOpen: React.Dispatch<React.SetStateAction<boolean>>;
@@ -27,27 +24,49 @@ export default function Lightbox({
             alt="product image"
           />
           <button
-            className={styles.btn__prev}
+            className={`${styles.btn__prev} ${styles.btn__arrow}`}
             disabled={imageIndex === 0}
             onClick={() => {
               setImageIndex((prev) => prev - 1);
             }}>
-            <img src={leftArrow} alt="previous image" />
+            <svg width="12" height="18" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M11 1 3 9l8 8"
+                stroke="#1D2026"
+                stroke-width="3"
+                fill="none"
+                fill-rule="evenodd"
+              />
+            </svg>
           </button>
           <button
-            className={styles.btn__next}
+            className={`${styles.btn__next} ${styles.btn__arrow}`}
             disabled={imageIndex === images.length - 1}
             onClick={() => {
               setImageIndex((prev) => prev + 1);
             }}>
-            <img src={rightArrow} alt="next image" />
+            <svg width="13" height="18" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="m2 1 8 8-8 8"
+                stroke="#1D2026"
+                stroke-width="3"
+                fill="none"
+                fill-rule="evenodd"
+              />
+            </svg>
           </button>
           <button
             className={styles.btn__exit}
             onClick={() => {
               setLightboxOpen(false);
             }}>
-            <img src={exitIcon} alt="exit icon" />
+            <svg width="14" height="15" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 4.597-2.122-2.122L4.878 7.5.282 2.904 2.404.782l4.595 4.596L11.596.782Z"
+                fill="#69707D"
+                fill-rule="evenodd"
+              />
+            </svg>
           </button>
         </div>
         <div className={styles.thumbnails}>
