d97e276abc25ac9290bea675c2925b8336c119b3
Ryan <ryanbprog@gmail.com>
2024-06-11 14:49:03 +0800

    cart styling done

--- src/components/cart-preview/CartPreview.tsx
+++ src/components/cart-preview/CartPreview.tsx
@@ -1,10 +1,53 @@
+import { CartItem, CartAction } from "../../utils/types/types";
 import styles from "./CartPreview.module.scss";
+import deleteIcon from "../../assets/images/icon-delete.svg";
+import productThumb from "../../assets/images/image-product-1-thumbnail.jpg";
 
-export default function CartPreview() {
+type CartItemProps = {
+  cartItem: CartItem;
+  cartDispatch: React.Dispatch<CartAction>;
+};
+
+function Item({ cartItem, cartDispatch }: CartItemProps) {
+  return (
+    <li>
+      <img src={productThumb} alt="cart item thumbnail" />
+      <div>
+        <span className={styles.product__title}>{cartItem.title}</span>
+        <div>
+          <span>${cartItem.price}.00</span>
+          <span>x{cartItem.qty}</span>
+          <span>$350.00</span>
+        </div>
+      </div>
+      <button>
+        <img src={deleteIcon} alt="delete icon" />
+      </button>
+    </li>
+  );
+}
+
+type CartPreviewProps = {
+  cartState: CartItem[];
+  cartDispatch: React.Dispatch<CartAction>;
+};
+
+export default function CartPreview({
+  cartState,
+  cartDispatch,
+}: CartPreviewProps) {
   return (
     <div className={styles.cart__cont}>
       <h3>Cart</h3>
-      <ul>cart items...</ul>
+      <ul>
+        {cartState.length > 0 ? (
+          cartState.map((item) => {
+            return <Item cartItem={item} cartDispatch={cartDispatch} />;
+          })
+        ) : (
+          <p>Your cart is empty</p>
+        )}
+      </ul>
       <button>Checkout</button>
     </div>
   );
